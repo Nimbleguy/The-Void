b788521d4b6a2bfd1d0056eb094326a40a78d435
Nimbleguy <Happyboy431@gmail.com>
2014-07-18 20:43:15 -0400

    Worked on the SPAWNENTITY attack.

--- aj/Java/Nullvoid/Entity/Attack/SpawnEntity.java
+++ aj/Java/Nullvoid/Entity/Attack/SpawnEntity.java
@@ -1,26 +1,72 @@
 package aj.Java.Nullvoid.Entity.Attack;
 
+import java.lang.reflect.Constructor;
 import aj.Java.Nullvoid.Entity.EntityGlitch;
 import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
 
 public class SpawnEntity implements IGlitchAttack {
 	
 	EntityGlitch glitch;
+	Class<? extends Entity> spawn;
 	
-	public SpawnEntity(EntityGlitch master, Entity toSpawn){
+	public SpawnEntity(EntityGlitch master, Class<? extends Entity> toSpawn){
 		glitch = master;
+		spawn = toSpawn;
 	}
 	
 	@Override
 	public void use(Entity target) {
-		// TODO Auto-generated method stub
-
+		try {
+			Constructor<? extends Entity> c = spawn.getDeclaredConstructor(World.class);
+			int[] coords = new int[3];
+			for(int i = 0; i < 8; i++){
+				coords[0] = (int) glitch.posX;
+				coords[1] = (int) glitch.posY;
+				coords[2] = (int) glitch.posZ;
+				switch(i){
+				case(0):
+					coords[0] -= 3;
+					break;
+				case(1):
+					coords[0] -= 2;
+					coords[2] -= 2;
+					break;
+				case(2):
+					coords[2] -= 3;
+					break;
+				case(3):
+					coords[0] += 2;
+					coords[2] -= 2;
+					break;
+				case(4):
+					coords[2] += 3;
+					break;
+				case(5):
+					coords[0] += 2;
+					coords[2] += 2;
+					break;
+				case(6):
+					coords[0] += 3;
+					break;
+				case(7):
+					coords[0] -= 2;
+					coords[2] += 2;
+					break;
+				}
+				Entity e = c.newInstance(glitch.worldObj);
+				e.setLocationAndAngles(coords[0], coords[1], coords[2], 0F, 0F);
+				glitch.worldObj.spawnEntityInWorld(e);
+			}
+		} catch (Exception e) {
+			System.err.println("There has been a fatal error with the Void Mod. Please report this to Nimbleguy.");
+			e.printStackTrace(System.err);
+		}
 	}
 
 	@Override
 	public int getTier() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 }
