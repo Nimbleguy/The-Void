0abba6aa42e10d2ba5a88cf9711544ab19c01b12
Nimbleguy <Happyboy431@gmail.com>
2014-07-23 12:55:09 -0400

    Hopefully fixed what the last commit fixed, agein.

--- aj/Java/Nullvoid/Utils.java
+++ aj/Java/Nullvoid/Utils.java
@@ -75,7 +75,7 @@ public class Utils {
 	}
 	@SuppressWarnings("unchecked")
 	public static EntityPlayerMP getPlayerFromUUID(String s){
-		if(MinecraftServer.getServer().getConfigurationManager().playerEntityList != null){
+		if(MinecraftServer.getServer().getConfigurationManager() != null && MinecraftServer.getServer().getConfigurationManager().playerEntityList != null){
 			for(EntityPlayerMP p : (List<EntityPlayerMP>)MinecraftServer.getServer().getConfigurationManager().playerEntityList){
 				if(s.matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")){
 					if(p.getUniqueID().compareTo(UUID.fromString(s)) == 0){
