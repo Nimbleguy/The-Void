998058a00ae990a6f29650c4fd2c796fe34cc25d
Nimbleguy <Happyboy431@gmail.com>
2014-07-29 23:33:32 -0400

    Worked with the Elemental hammer custom render.

--- aj/Java/Nullvoid/client/render/item/render/RenderHammer.java
+++ aj/Java/Nullvoid/client/render/item/render/RenderHammer.java
@@ -11,92 +11,44 @@ import net.minecraftforge.client.IItemRenderer;
 
 public class RenderHammer implements IItemRenderer {
 	private ModelHammer model;
-	private Minecraft mc = Minecraft.getMinecraft();
-	private float scale = 1F;
 	
 	public RenderHammer() {
         model = new ModelHammer();
     }
 	
 	@Override
-    public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-        switch (type) {
+	public boolean handleRenderType(ItemStack item, ItemRenderType type)
+	{
+		return true;
+	}
 
-        case EQUIPPED:
-            GL11.glPushMatrix();
-            mc.renderEngine.bindTexture(new ResourceLocation(
-                    "subaraki:weapons/Sword.png"));
-            //GL11.glRotatef(0F, 1.0f, 0.0f, 0.0f);
-            GL11.glRotatef(-5F, 0.0f, 1.0f, 0.0f);
-            GL11.glRotatef(-150F, 0.0f, 0.0f, 1.0f);
-            GL11.glTranslatef(-0.8F, 0.6F, -0.1F);
-            model.render((Entity) data[1], 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
-                    0.0625F);
-            GL11.glPopMatrix();
-            break;
+	@Override
+	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
+			ItemRendererHelper helper) 
+	{
+		return true;
+	}
 
-        case EQUIPPED_FIRST_PERSON:
-            GL11.glPushMatrix();
-            mc.renderEngine.bindTexture(new ResourceLocation(
-                    "subaraki:weapons/Sword.png"));
-            //GL11.glRotatef(0F, 1.0f, 0.0f, 0.0f);
-            GL11.glRotatef(-5F, 0.0f, 1.0f, 0.0f);
-            GL11.glRotatef(-150F, 0.0f, 0.0f, 1.0f);
-            GL11.glTranslatef(-0.8F, 0.9F, -0.1F);
-            model.render((Entity) data[1], 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
-                    0.0625F);
-            GL11.glPopMatrix();
-            break;
-
-        case ENTITY:
-            GL11.glPushMatrix();
-            scale = 1.5F;
-            GL11.glScalef(scale, scale, scale);
-            mc.renderEngine.bindTexture(new ResourceLocation(
-                    "subaraki:weapons/Sword.png"));
-            GL11.glRotatef(90F, 1.0f, 0.0f, 0.0f);
-            GL11.glRotatef(0F, 0.0f, 1.0f, 0.0f);
-            GL11.glRotatef(45F, 0.0f, 0.0f, 1.0f);
-            GL11.glTranslatef(-0.2F, 1F, 0F);
-            model.render((Entity) data[1], 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,
-                    0.0625F);
-            GL11.glPopMatrix();
-            break;
-
-        case INVENTORY:
-            GL11.glPushMatrix();
-            scale = 0.7F;
-            GL11.glScalef(scale, scale, scale);
-            mc.renderEngine.bindTexture(new ResourceLocation(
-                    "subaraki:weapons/Sword.png"));
-
-            GL11.glRotatef(200F, 1.0f, 0.0f, 0.0f);
-            GL11.glRotatef(-80F, 0.0f, 1.0f, 0.0f);
-            //GL11.glTranslatef(0.0F, 1.2F, 0F);
-            model.render(0.0625F);
-            GL11.glPopMatrix();
-            break;
-
-        default:
-            break;
-        }
-    }
-@Override
-    public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-        return true;
-    }
-
-public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
-            ItemRendererHelper helper) {
-
-        switch (type) {
-        case INVENTORY:
-            return true;
-        default:
-            break;
-        }
-        return false;
-
-    }
+	@Override
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data) 
+	{
+		GL11.glPushMatrix();
+		GL11.glScalef(2F, 2F, 2F);
+		GL11.glRotatef(67.5F, 0F, 1F, 0F);
+		if(type.compareTo(ItemRenderType.EQUIPPED_FIRST_PERSON) == 0){
+			GL11.glTranslatef(0.25F, 0F, 0F);
+		}
+		else{
+			GL11.glTranslatef(0F, 0F, -0.5F);
+		}
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("nullvoid:textures/models/hammer/default"));
+		if(data.length > 1){
+			model.render((Entity)data[1], 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0625f);
+		}
+		else{
+			model.render(null, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0625f);
+		}
+		GL11.glPopMatrix();
+	}
 
 }
