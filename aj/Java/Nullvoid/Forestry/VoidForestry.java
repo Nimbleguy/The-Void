8aeca68c02ab7156d033c4ec49e2e454167479d6
Nimbleguy <Happyboy431@gmail.com>
2014-08-17 15:01:28 -0400

    Worked on bees.

--- aj/Java/Nullvoid/Forestry/VoidForestry.java
+++ aj/Java/Nullvoid/Forestry/VoidForestry.java
@@ -1,11 +1,18 @@
 package aj.Java.Nullvoid.Forestry;
 
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.util.EnumHelper;
+import forestry.api.apiculture.EnumBeeChromosome;
+import forestry.api.core.EnumHumidity;
+import forestry.api.core.EnumTemperature;
 import forestry.api.genetics.IAllele;
 import forestry.api.genetics.IClassification.EnumClassLevel;
 import forestry.api.genetics.IMutation;
 import forestry.apiculture.genetics.AlleleBeeSpecies;
 import forestry.apiculture.genetics.BeeMutation;
+import forestry.apiculture.genetics.BeeTemplates;
 import forestry.core.genetics.Allele;
+import forestry.core.genetics.AllelePlantType;
 import forestry.core.genetics.AlleleSpecies;
 import forestry.core.genetics.Classification;
 
@@ -16,19 +23,49 @@ public class VoidForestry {
 	public static AlleleSpecies allVoidB;
 	public static AlleleSpecies allNullB;
 	
+	public static AllelePlantType fluxFlower;
+	public static EnumPlantType fluxPlant;
+	
 	public static Classification voidClass;
 	
 	public VoidForestry(){
-		System.out.println("BEEEEE");
 		registerBees();
 	}
 	
 	public static void registerBees(){
+		Class<AllelePlantType> c = AllelePlantType.class;
+		try {
+			fluxFlower = c.getConstructor(String.class, EnumPlantType.class, Boolean.class).newInstance("flux", fluxPlant = EnumHelper.addEnum(EnumPlantType.class, "FLUX"), true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		voidClass = new Classification(EnumClassLevel.GENUS, "nullvoid", "Nihil Solutionibus");
+		voidClass.addMemberGroup(new Classification(EnumClassLevel.TRIBE, "basenullvoid", "Fundatur Ciphra"));
 		
-		allNullB = new AlleleBeeSpecies("null", true, "Null", voidClass, "Nullus", 0x6600FF, 0xFFFFFF);
-		allNullB = new AlleleBeeSpecies("void", true, "Void", voidClass, "Vacuos", 0xFFFFFF, 0x6600FF);
+		allNullB = new AlleleBeeSpecies("null", false, "Nulled", voidClass, "Nullus", 0x6600FF, 0xFFFFFF);
+		allNullB.setHasEffect();
+		allNullB.setHumidity(EnumHumidity.ARID);
+		allNullB.setTemperature(EnumTemperature.ICY);
 		
-		nullBMut = new BeeMutation(Allele.speciesEdenic, Allele.speciesEnded, new IAllele[] {Allele.speciesEdenic, Allele.speciesEnded}, 1);
+		
+		allVoidB = new AlleleBeeSpecies("void", true, "Voided", voidClass, "Vacuos", 0xFFFFFF, 0x6600FF);
+		
+		nullBMut = new BeeMutation(Allele.speciesEdenic, Allele.speciesEnded, getNullTemplate(), 1);
+	}
+	
+	public static IAllele[] getNullTemplate() {
+		IAllele[] alleles = BeeTemplates.getDefaultTemplate();
+		alleles[EnumBeeChromosome.SPEED.ordinal()] = Allele.speedSlower;
+		alleles[EnumBeeChromosome.LIFESPAN.ordinal()] = Allele.lifespanShortest;
+		alleles[EnumBeeChromosome.FERTILITY.ordinal()] = Allele.fertilityLow;
+		alleles[EnumBeeChromosome.FLOWERING.ordinal()] = Allele.floweringSlowest;
+		alleles[EnumBeeChromosome.HUMIDITY_TOLERANCE.ordinal()] = Allele.toleranceNone;
+		alleles[EnumBeeChromosome.TEMPERATURE_TOLERANCE.ordinal()] = Allele.toleranceNone;
+		alleles[EnumBeeChromosome.CAVE_DWELLING.ordinal()] = Allele.boolTrue;
+		alleles[EnumBeeChromosome.FLOWER_PROVIDER.ordinal()] = fluxFlower;
+		alleles[EnumBeeChromosome.EFFECT.ordinal()] = Allele.effectAggressive;
+		alleles[EnumBeeChromosome.SPECIES.ordinal()] = allNullB;
+		return alleles;
 	}
 }
